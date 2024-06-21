22ce2033802dac09abcb6b35e2ae436b1a5ce896
livingproof18 <livingking21@gmail.com>
2024-06-21 20:21:39 +0100

    goat code created and data collected with ctz

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path = require('path');
 const axios = require('axios');
 
 
-const BRAND_NAME = "Louis Vuitton";
-const CATEGORY_VALUE = "Jacket";
+const BRAND_NAME = "Corteiz";
+const CATEGORY_VALUE = "TOP";
 const GENDER = "Mens";
 
 async function getProductDetails(url) {
@@ -13,7 +13,7 @@ async function getProductDetails(url) {
     const browser = await puppeteer.launch({
         headless: false,
         // headless: 'new',
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--ignore-certificate-errors', '--enable-http2', '--disable-web-security'],
         ignoreHTTPSErrors: true
     });
 
@@ -25,15 +25,20 @@ async function getProductDetails(url) {
     await page.goto(url, { waitUntil: 'networkidle2' });
 
     // Handle cookie modal
+    try {
+        await page.waitForSelector('#onetrust-banner-sdk > div', { timeout: 5000 }); // Change selector to match the actual cookie modal
+        await page.click('#onetrust-accept-btn-handler'); // Change selector to match the actual accept button
+    } catch (error) {
+        console.log('No cookie modal found or failed to accept cookies:', error);
+    }
+    // Change country modal
+    // Handle change country modal
     // try {
-    //     await page.waitForSelector('#ucm-details > div.ucm-popin.ucm-popin--pushpop.ucm-popin--active > div', { timeout: 5000 }); // Change selector to match the actual cookie modal
-    //     await page.click('#ucm-details > div.ucm-popin.ucm-popin--pushpop.ucm-popin--active > div > form > ul > li:nth-child(3) > button'); // Change selector to match the actual accept button
+    //     await page.waitForSelector('#modalContent', { timeout: 5000 });
+    //     await page.click('#modalContent > div.lv-localize-modal__other-wrapper > button');
     // } catch (error) {
-    //     console.log('No cookie modal found or failed to accept cookies:', error);
+    //     console.log('No region modal found or failed to accept region selection:', error);
     // }
-    // // Change country modal
-
-    await new Promise(resolve => setTimeout(resolve, 2000)); // Wait for images to load
 
 
     // Scroll down to load all images
@@ -45,55 +50,57 @@ async function getProductDetails(url) {
 
     // Wait for the specific elements to ensure they are loaded
     // Cwith fugazi I have to keep chaning the image ids--- longgggg
-    await page.waitForSelector('#main > div.lv-product > div > section > div.lv-product-page-header__primary > div > div > ul > li:nth-child(2) > div > div > picture > img');
+    await page.waitForSelector('#main > div > div.swiper-slide.swiper-slide-active > div > img');
     // await page.waitForSelector('#ProductImage-36340024475820');
 
     const details = await page.evaluate((BRAND_NAME, CATEGORY_VALUE, GENDER) => {
         const brand = BRAND_NAME;
-        const name = document.querySelector(".lv-product__name")?.innerText.trim();
+        const name = document.querySelector(".ProductInfo__Name-sc-yvcr9v-2")?.innerText.trim();
 
         const category = CATEGORY_VALUE;
-        const priceText = document.querySelector('.lv-product__price > span')?.innerText.trim();
+        const priceText = document.querySelector('#main-page-layout > div > div.ProductDetailPageDesktop__FlexContainer-sc-1l0dpjw-1.kGdLAA > footer > div.ProductTemplateDesktopFooterUpdated__FooterTop-sc-ake9f7-1.fZCCxJ > div.BigBuyBarDesktop__Root-sc-xdkwux-0.llszQj > div > div.BigBuyBarDesktopSwiperSection__Wrapper-sc-196rppg-0.igNAlG > div > div > div:nth-child(3) > div > span')?.innerText.trim();
         const price = priceText ? parseFloat(priceText.replace(/[^\d.-]/g, '')) : null;
         const gender = GENDER;
-        const description = document.querySelector("#main > div.lv-product > section > div.lv-product-seo-details > p")?.innerText.trim();
-        const color = document.querySelector("#main > div.lv-product > section > div.lv-product-seo-details > div > div > div > ul:nth-child(1) > li:nth-child(2)")?.innerText.trim();
+        // const description = document.querySelector("#main > div.lv-product > section > div.lv-product-seo-details > p")?.innerText.trim();
+        const description = '';
+        const color = document.querySelector("#pdp-wrapper > div > div.FactsWindow__Wrapper-sc-1hjbbqw-1.bnCzVo > div:nth-child(6) > span.WindowItemShortText__Right-sc-jrzdw-2.XUSFT > button > span")?.innerText.trim();
 
         const imagesUrl = [];
         const imageNames = [];
         // /Get first Image,
         // console.log("getting image")
 
-        const onlyImage = document.querySelector("#image-container4 > div > picture > img");
+        const firstImage = document.querySelector("#main > div > div.swiper-slide.swiper-slide-active > div > img");
 
-        if (onlyImage) {
-            const srcset = onlyImage.getAttribute('srcset');
+        if (firstImage) {
+            const srcset = firstImage.getAttribute('srcset');
             if (srcset) {
-                const firstSrc = srcset.split(',')[4].trim().split(' ')[0]; // Get the first srcset URL
+                const firstSrc = srcset.split(',')[19].trim().split(' ')[0]; // Get the first srcset URL
                 imagesUrl.push(firstSrc);
-                const imageName = `${brand.replace(/\s+/g, '-')}-${name.replace(/\s+/g, '-')}-0`;
+                // const imageName = `${brand.replace(/\s+/g, '-')}-${name.replace(/\s+/g, '-')}-0`;
+                const imageName = `${brand.replace(/\s+/g, '-').replace(/\//g, '-').replace(/'/g, '')}-${name.replace(/\s+/g, '-').replace(/\//g, '-').replace(/'/g, '')}-0`;
                 imageNames.push(imageName);
             } else {
-                imagesUrl.push(onlyImage.src);
-                const imageName = `${brand.replace(/\s+/g, '-')}-${name.replace(/\s+/g, '-')}-0`;
+                imagesUrl.push(firstImage.src);
+                const imageName = `${brand.replace(/\s+/g, '-').replace(/\//g, '-').replace(/'/g, '')}-${name.replace(/\s+/g, '-').replace(/\//g, '-').replace(/'/g, '')}-0`;
                 imageNames.push(imageName);
             }
         }
-        // const secondImage = document.querySelector("#owl-carousel-gallery > div.owl-stage-outer > div > div:nth-child(2) > div > a > img");
-        // if (secondImage) {
-        //     const srcset = secondImage.getAttribute('srcset');
-        //     if (srcset) {
-        //         const firstSrc = srcset.split(',')[7].trim().split(' ')[0]; // Get the first srcset URL
-        //         imagesUrl.push(firstSrc);
-        //         const imageName = `${brand.replace(/\s+/g, '-')}-${name.replace(/\s+/g, '-')}-1`;
-        //         imageNames.push(imageName);
-        //     }
-        //     else {
-        //         imagesUrl.push(secondImage.src);
-        //         const imageName = `${brand.replace(/\s+/g, '-')}-${name.replace(/\s+/g, '-')}-1`;
-        //         imageNames.push(imageName);
-        //     }
-        // }
+        const secondImage = document.querySelector("#main > div > div.swiper-slide.swiper-slide-next > div > img");
+        if (secondImage) {
+            const srcset = secondImage.getAttribute('srcset');
+            if (srcset) {
+                const firstSrc = srcset.split(',')[19].trim().split(' ')[0]; // Get the first srcset URL
+                imagesUrl.push(firstSrc);
+                const imageName = `${brand.replace(/\s+/g, '-').replace(/\//g, '-').replace(/'/g, '')}-${name.replace(/\s+/g, '-').replace(/\//g, '-').replace(/'/g, '')}-1`;
+                imageNames.push(imageName);
+            }
+            else {
+                imagesUrl.push(secondImage.src);
+                const imageName = `${brand.replace(/\s+/g, '-').replace(/\//g, '-').replace(/'/g, '')}-${name.replace(/\s+/g, '-').replace(/\//g, '-').replace(/'/g, '')}-1`;
+                imageNames.push(imageName);
+            }
+        }
         // const thirdImage = document.querySelector("#slider > div > div > div > div:nth-child(2) > div > div > img");
         // if (thirdImage) {
         //     const srcset = thirdImage.getAttribute('srcset');
@@ -118,8 +125,8 @@ async function getProductDetails(url) {
             gender: gender,
             description: description,
             color: color,
-            from: brand,
-            info: '',
+            from: 'GOAT',
+            info: 'Retail Price',
             'image': imageNames,
             'imagesUrl': imagesUrl,
             // Debug: {
@@ -198,10 +205,9 @@ async function autoScroll(page) {
 
 (async () => {
     const urls = [
-        'https://uk.louisvuitton.com/eng-gb/products/leather-blouson-nvprod4920020v/1AFAJW',
-        'https://uk.louisvuitton.com/eng-gb/products/monogram-printed-denim-shorts-nvprod5460015v/1AFQFD',
-        'https://uk.louisvuitton.com/eng-gb/products/embroidered-denim-blouson-nvprod4920008v/1AF774',
-        'https://uk.louisvuitton.com/eng-gb/products/mix-leather-varsity-blouson-nvprod4940204v/1AFIQA'
+        'https://www.goat.com/en-gb/apparel/corteiz-club-rtw-football-jersey-blue-7892-1ss230109crfj-blue',
+        'https://www.goat.com/en-gb/apparel/corteiz-club-rtw-football-jersey-black-7892-1fw230109crfj-blac',
+        'https://www.goat.com/en-gb/apparel/corteiz-mula-guerillaz-cargos-black-green-7892-1ss220210mgc-blac'
         // 'https://uk.louisvuitton.com/eng-gb/products/monogram-printed-denim-shorts-nvprod5460015v/1AFQFD'
         // Add more URLs as needed
     ];
